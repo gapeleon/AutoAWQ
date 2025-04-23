433816425453f5f980ccc1909af74dd289ae3514
Kohei ISHIZAKI <ishizaki@superstring.dev>
2025-04-23 19:54:43 +0000

    fix debug

--- awq/utils/llama4_extra_utils.py
+++ awq/utils/llama4_extra_utils.py
@@ -2,17 +2,20 @@ import functools
 from collections import defaultdict
 from tqdm import auto as tqdm_lib
 
+import torch
 from awq.quantize.quantizer import AwqQuantizer
-from awq.quantize.scale import apply_clip
-from awq.utils import clear_memory, get_best_device
+from awq.quantize.scale import apply_scale, apply_clip
+from awq.models.llama4 import Llama4TextMoe
+from awq.utils.utils import clear_memory, get_best_device
 from awq.utils.calib_data import get_calib_dataset
 from awq.modules.act import ScaledActivation
 from transformers.activations import ACT2FN
 
 from transformers.models.llama4.modeling_llama4 import Llama4TextRMSNorm
+from transformers.models.llama4.modeling_llama4 import Llama4TextMoe as OldLlama4TextMoe
 from transformers.feature_extraction_utils import BatchFeature
 
-from module import (
+from awq.utils.module import (
     append_str_prefix,
     get_op_name,
     get_op_by_name, 
@@ -22,81 +25,16 @@ from module import (
 )
 
 class Llama4AwqQuantizer(AwqQuantizer):
-        
-    @torch.no_grad()
-    def init_quant(self, n_samples=128, max_seq_len=512):
-        modules = self.awq_model.get_model_layers(self.model)
-        samples = get_calib_dataset(
-            data=self.calib_data,
-            tokenizer=self.tokenizer,
-            n_samples=n_samples,
-            max_seq_len=max_seq_len,
-            split=self.split,
-            text_column=self.text_column,
-        )
-        samples = torch.cat(samples, dim=0)
-
-        inps = []
-        layer_kwargs = {}
-
-        best_device = get_best_device()
-        modules[0] = modules[0].to(best_device)
-        self.awq_model.move_embed(self.model, best_device)
-
-        # get input and kwargs to layer 0
-        # with_kwargs is only supported in PyTorch 2.0
-        # use this Catcher hack for now
-        class Catcher(nn.Module):
-            def __init__(self, module):
-                super().__init__()
-                self.module = module
-
-            def forward(self, *args, **kwargs):
-                # assume first input to forward is hidden states
-                if len(args) > 0:
-                    hidden_states = args[0]
-                    del args
-                else:
-                    first_key = list(kwargs.keys())[0]
-                    hidden_states = kwargs.pop(first_key)
-
-                inps.append(hidden_states)
-                layer_kwargs.update(kwargs)
-                raise ValueError  # early exit to break later inference
-
-        # patch layer 0 to catch input and kwargs
-        modules[0] = Catcher(modules[0])
-        try:
-            self.model(samples.to(next(self.model.parameters()).device))
-        except ValueError:  # work with early exit
-            pass
-        modules[0] = modules[0].module  # restore
-
-        # Update the layer kwargs with `prepare_inputs_for_generation` method
-        # that takes care of everything to avoid unexpected errors.
-        layer_kwargs = self.model.prepare_inputs_for_generation(samples, **layer_kwargs)
-        # Pop the input_ids as they are not needed at all.
-        layer_kwargs.pop("input_ids")
-
-        del samples
-        inps = inps[0]
-
-        modules[0] = modules[0].cpu()
-        self.awq_model.move_embed(self.model, "cpu")
-
-        clear_memory()
-
-        if layer_kwargs.get("attention_mask") is not None:
-            layer_kwargs["attention_mask"] = layer_kwargs["attention_mask"].to(
-                best_device
-            )
-
-        return modules, layer_kwargs, inps
 
     def _preprocess(self):
         pass
 
     def _preprocess_layer_iter(self, layer_index):
+        self.modules[layer_index].load_state_dict(
+            torch.load(f"/workspace/hf_cache/marverick/lang_layer{layer_index:03}.pth"),
+            assign=True,
+        )
+        self.modules[layer_index] = self.modules[layer_index].to(torch.float16)
         if isinstance(self.modules[layer_index].feed_forward, OldLlama4TextMoe):
             self.modules[layer_index].feed_forward = Llama4TextMoe.replace(self.modules[layer_index].feed_forward.to('cpu'))
         
@@ -110,23 +48,23 @@ class Llama4AwqQuantizer(AwqQuantizer):
             self.modules[layer_index] = self.modules[layer_index].to(best_device)
             common_device = next(self.modules[layer_index].parameters()).device
 
-        for k in self.inps_dict.keys():
-            if self.module_kwargs_dict[k].get("position_ids") is not None:
-                self.module_kwargs_dict[k]["position_ids"] = self.module_kwargs_dict[k][
-                    "position_ids"
-                ].to(common_device)
-    
-            if self.module_kwargs_dict[k].get("attention_mask") is not None:
-                self.module_kwargs_dict[k]["attention_mask"] = self.module_kwargs_dict[k][
-                    "attention_mask"
-                ].to(common_device)
-    
-            self.inps_dict[k] = self.inps_dict[k].to("cpu")
+        if self.module_kwargs.get("position_ids") is not None:
+            self.module_kwargs["position_ids"] = self.module_kwargs[
+                "position_ids"
+            ].to(common_device)
+
+        if self.module_kwargs.get("attention_mask") is not None:
+            self.module_kwargs["attention_mask"] = self.module_kwargs[
+                "attention_mask"
+            ].to(common_device)
+
+        self.inps = self.inps.to(common_device)
 
     def _postprocess_layer_iter(self, layer_index):
         self.modules[layer_index] = self.modules[layer_index].to("cpu")
-        torch.save(self.modules[layer_index].state_dict(), f"/workspace/mllama-autoawq/tmp/q{layer_index:04}.pt")
+        torch.save(self.modules[layer_index].state_dict(), f"/workspace/hf_cache/marverick/q{layer_index:03}.pt")
         self.modules[layer_index] = None
+        clear_memory()
     
     def quantize(self):
         self._preprocess()
